959a7e7b7653262e26e62577f0c598d8d172d40f
HenkSeed <adaner@inbox.ru>
2026-01-29 22:57:05 +0700

    Day 2. Accordeon, modal and burger

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+const modalButtons = document.querySelectorAll('.more')
+
+const modal = document.querySelector('.modal')
+
+const overlay = modal.querySelector('.overlay')
+
+const closeButton = document.querySelector('.modal__close')
+
+modalButtons.forEach((modalButton) => {
+    modalButton.addEventListener('click', () => {
+        modal.classList.remove('hidden')
+    })
+})
+
+overlay.addEventListener('click', () => {
+    modal.classList.add('hidden')
+})
+
+closeButton.addEventListener('click', () => {
+    modal.classList.add('hidden')
+})
